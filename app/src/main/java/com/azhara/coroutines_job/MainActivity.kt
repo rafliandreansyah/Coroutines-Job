78fed55709eb9e4e138d868f6d0e7d62f2e71201
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-04-10 16:43:59 +0700

    memberikan fungsi job invokeOnCompletion agar memberi tau segala aktifitas pada job dan method ini akan selalu dijalan sesusai event dari job tersebut

--- app/src/main/java/com/azhara/coroutines_job/MainActivity.kt
+++ app/src/main/java/com/azhara/coroutines_job/MainActivity.kt
@@ -2,9 +2,9 @@ package com.azhara.coroutines_job
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CompletableJob
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,12 +21,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btn_start.setOnClickListener {
+            if (!::job.isInitialized){
+                initJob()
+            }
+        }
     }
 
     private fun initJob(){
         btn_start.text = "Start Job#1"
         tv_job_status.text = ""
         job = Job()
+
+        // fungsi spesial pada job dimana saat job berjalan dapat memberitahu aktifitas apapun seperti cancel job, memperbarui loading dll ataupun job selesai dijalankan
+        // fungsi ini akan selalu di eksekusi sesuai permintaan
+        job.invokeOnCompletion {
+            it?.message.let {
+                var msg = it
+                if (msg.isNullOrBlank()){
+                    msg = "Unknown cancellation error"
+                }
+                println("$job was cancelled, reason: $msg")
+                showToast(msg)
+            }
+        }
+
+        job_progress.min = PROGRESS_MIN
+        job_progress.max = PROGRESS_MAX
+    }
+
+    private fun showToast(msg: String) {
+        GlobalScope.launch(Dispatchers.Main){
+            Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
